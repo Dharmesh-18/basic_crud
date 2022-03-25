e84556f83b6f73c5391f859c43c156d852cec669
Dharmesh18 <dharmesh.tiwari@credable.in>
2022-03-25 18:07:52 +0530

    added student service and started to reference the methods from their only

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,17 @@ import java.util.List;
 @RequestMapping("/api/student")
 public class StudentController {
 
+    @Autowired
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService =  studentService;
+    }
+
+
     @GetMapping
     public List<Student> getStudents(){
-        return List.of(new Student(1L, "abc", "abc@xyz.com"));
+        return studentService.getStudents();
     }
 
 
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,15 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+
+    public List<Student> getStudents() {
+        return List.of(new Student(1L, "abc", "abc@xyz.com"));
+    }
+}
