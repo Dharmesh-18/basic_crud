272f8773e0f73914506a37b2c2668ae199f4357a
Dharmesh18 <dharmesh.tiwari@credable.in>
2022-03-26 20:13:39 +0530

    connection with databse and added repository file, then referenced it through service class..also added application properties for connecting with database

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,5 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +7,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
 
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "abc", "abc@xyz.com"));
+        return studentRepository.findAll();
     }
 }
